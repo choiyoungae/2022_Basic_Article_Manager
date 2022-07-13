6776637c17f2013778ea794785735100da7f4be7
choiyoungae <choiaeyoung1001@gmail.com>
2022-07-13 18:52:54 +0900

    게시물 작성 기능 구현2

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -16,7 +16,7 @@ public class Main {
 
 		while (true) {
 			System.out.printf("명령어) ");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 			
 			if(cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요.");
@@ -32,12 +32,10 @@ public class Main {
 				if(articleNumber == 0) {					
 					System.out.println("게시글이 없습니다.");
 				} else {
-					for(int i=0; i<articleNumber; i++) {
+					System.out.println("번호 / 제목");
+					for(int i=articleNumber-1; i>=0; i--) {
 						Article thisArticle = articles.get(i);
-						System.out.printf("=== %d번 글 ===\n", thisArticle.articleNumber);
-						System.out.printf("제목 : %s\n", thisArticle.title);
-						System.out.printf("내용 : %s\n", thisArticle.body);
-						System.out.println("=============");
+						System.out.printf("%s   / %s\n", thisArticle.articleNumber, thisArticle.title);
 					}
 				}
 				
@@ -48,6 +46,7 @@ public class Main {
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
+				
 				Article article = new Article(title, body, articleNumber);
 				articles.add(article);
 				System.out.printf("%d번 글이 생성되었습니다.\n", articleNumber);
